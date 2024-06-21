76877923d2ad433029b55bdd131730241dee3988
Saraí Rojas García <rojasg.sarai@gmail.com>
2024-06-20 21:00:50 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+//scroll top selection
+const scrollUp = document.querySelector("#scroll-up")
+
+//Function
+scrollUp.addEventListener("click", () =>
+     {window.scrollTo({top:0,
+       left:0, behavoir:"smooth"});
+      })
